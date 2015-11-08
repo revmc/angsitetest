96ea56de61fc25e9a10863e745b7aac1f16452f6
revmc <shaque9000@gmail.com>
2015-11-08 01:15:44 -0500

    Adding a service for the experience data

--- scripts/controllers/experienceController.js
+++ scripts/controllers/experienceController.js
@@ -1,30 +1,6 @@
 angular.module('resumeApp')
-.controller('expCtrl', ['$scope', function($scope) {
+.controller('expCtrl', ['$scope', 'ExpService', function($scope, ExpService) {
 	
-	$scope.data = {
-    	title: 'Experience',
-    	experienceItems: [
-    		{
-    			employer: 'Qlik',
-    			start: '7/13/15',
-    			end: 'Present',
-    			jobTitle: 'Front End JavaScript Developer',
-    			description: 'Building custom Qlik Sense extensions in JavaScript/CSS/HTML; Building mashups embedding Qlik Sense charts into Web sites'
-    		},
-    		{
-    			employer: 'Grassroots Technologies, Inc.',
-    			start: '1/2009',
-    			end: '5/2015',
-    			jobTitle: 'Developer',
-    			description: 'Maintained and coded standalone and web-based software in Flash using ActionScript, JavaScript, HTML and CSS; implemented unit testing with ASUnit; modified Ant and SQL scripts; trained and code reviewed junior developers.'
-    		}/*,
-    		{
-    			employer: '',
-    			start: '',
-    			end: '',
-    			jobTitle: '',
-    			description: ''
-    		}*/
-    	]
-    }
+	$scope.data = ExpService.data;
+
 }]);
