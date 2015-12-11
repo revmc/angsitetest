4b3de4643dfa78185bd1634a248db5cc05d0eff9
revmc <shaque9000@gmail.com>
2015-12-11 01:39:15 -0500

    adding concat functionality in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
   grunt.initConfig(
     {
       pkg: grunt.file.readJSON('package.json'),
+
+      concat: {
+        dist: {
+          src: [
+            'scripts/controllers/experienceController.js',
+            'scripts/controllers/educationController.js',
+            'scripts/controllers/skillsController.js',
+            'scripts/services/expService.js',
+            'scripts/services/eduService.js',
+            'scripts/services/skillsService.js'
+          ],
+          dest: 'build/scripts/controllers_and_services.js'
+        }
+      },
+
       jshint: {
         all: ['scripts/**/*.js']
       }
@@ -10,9 +25,10 @@ module.exports = function(grunt) {
   );
   
   // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['concat']);
   
 };
